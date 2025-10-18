2b67e38a529e1d10230b6e8d75b3a73b5680436c
Marco Predari <predorock@gmail.com>
2025-10-18 18:39:40 +0200

    fixed slider color

--- src/components/ui/Slider.jsx
+++ src/components/ui/Slider.jsx
@@ -18,7 +18,7 @@ export default function SliderComponent({ min = 0, max = 100, step = 1, value, o
             <div className='flex flex-row align-center justify-center p-6 border-1 border-slate-200 rounded-md mb-4 shadow-[0_4px_8px_rgba(0,0,0,0.1)]'>
                 <span className='font-enel-bold text-base text-secondary'>{value}</span>&nbsp;m²
             </div>
-            <div className="rounded-full bg-slate-400 py-2 pl-2 pr-4">
+            <div className="rounded-full bg-slate-300 py-2 pl-2 pr-4">
                 <Slider {...sliderProps} value={value} />
             </div>
         </div>
